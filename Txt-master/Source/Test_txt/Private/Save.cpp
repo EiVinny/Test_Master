8147129eddd8903aa4b4877330b47059248772b8
EiVinny <vinnyosantos@gmail.com>
2018-05-29 08:29:52 -0300

    1.3

---
+++ Txt-master/Source/Test_txt/Private/Save.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Save.h"
+
+Save::Save()
+{
+}
+
+Save::~Save()
+{
+}
---
+++ Txt-master/Source/Test_txt/Public/Save.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "iostream"
+#include "windows.system.h"
+#include "ActorComponent.generated.h"
+
+using namespace std;
+
+/**
+ * 
+ */
+class TEST_TXT_API Save{
+	private:
+		BinaryParam bf = new BinaryParam();
+	public:
+		Save();
+		static void SaveInfo (AActor dados,string path)
+		~Save();
+};
